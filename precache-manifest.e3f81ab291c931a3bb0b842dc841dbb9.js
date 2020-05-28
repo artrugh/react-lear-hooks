8946a54df706a6ab05259b1df41e571b7eb648bd
artrugh <arturo.rugh@gmail.com>
2020-05-28 16:44:04 +0200

    Updates

---
+++ precache-manifest.e3f81ab291c931a3bb0b842dc841dbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e075055e99e2f062c8ba73e007847717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde872a44b7801554dd0",
+    "url": "/static/css/main.0a435548.chunk.css"
+  },
+  {
+    "revision": "f26bc64c963a992d853e",
+    "url": "/static/js/2.b8af44be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8af44be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde872a44b7801554dd0",
+    "url": "/static/js/main.943cfc69.chunk.js"
+  },
+  {
+    "revision": "bef4b35d5aaf3de8ab03",
+    "url": "/static/js/runtime-main.e09c0c2e.js"
+  }
+]);
